385a88f8bd00acbc4a37d4c595959444a400e13f
Takoh Israel <iminuifuong@gmail.com>
2025-12-07 13:13:21 +0000

    latest built front

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,15 @@
-
 import React, { useEffect } from 'react';
 import './Loader.css';
 
 const Loader = ({ poweredBy }) => (
   <div className="loader-root">
-    <div className="loader-dots">
-      <span className="dot dot-blue"></span>
-      <span className="dot dot-darkblue"></span>
-      <span className="dot dot-orange"></span>
+    <div className="loader-spinner">
+      <div className="spinner-ring ring-blue"></div>
+      <div className="spinner-ring ring-darkblue"></div>
+      <div className="spinner-ring ring-orange"></div>
     </div>
     <div className={`loader-powered-by${poweredBy ? ' loader-powered-by--visible' : ''}`}>Powered by Izzy Tech Team...</div>
   </div>
 );
 
-export default Loader; 
+export default Loader;
