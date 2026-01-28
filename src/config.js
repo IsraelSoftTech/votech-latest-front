d7b391d0234249d809629df510f6de977b0547cb
IsraelSoftTech <iminuifuong@gmail.com>
2026-01-28 12:25:47 +0100

    latest config.js api fix

--- src/config.js
+++ src/config.js
@@ -1,23 +1,4 @@
-const LOCAL_IP = "192.168.0.100";
-
-const ENV = process.env.REACT_APP_NODE_ENV || "production";
-
-const isDevelopment = ENV === "development";
-const isDesktop = ENV === "desktop";
-
-// Default API base URLs if environment variables are not set
-const DEFAULT_API_BASE_URL_DEV = "http://localhost:5000";
-const DEFAULT_API_BASE_URL_DESKTOP = `http://${LOCAL_IP}:5000`;
-const DEFAULT_API_BASE_URL_PROD = "https://api.votechs7academygroup.com";
-
-const API_BASE_URL = isDevelopment
-  ? process.env.REACT_APP_API_URL_DEV || DEFAULT_API_BASE_URL_DEV
-  : isDesktop
-  ? process.env.REACT_APP_API_URL_DESKTOP || DEFAULT_API_BASE_URL_DESKTOP
-  : process.env.REACT_APP_API_URL_PROD || DEFAULT_API_BASE_URL_PROD;
-
 const config = {
-  API_BASE_URL,
+  API_BASE_URL: "https://api.votechs7academygroup.com/api",
 };
-
 export default config;
